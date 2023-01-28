bbdb894c049b47115f1b09d2cbf997dfa90bd382
JIH0LEE <skql5810@gmail.com>
2023-01-28 12:47:51 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const userNameHandler = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+  };
+  const testHandler = () => {
+    const body = {
+      username: username,
+      password: password,
+    };
+    axios.post("http://localhost:8080/api/sign-in", body).then((res) => {
+      alert(res.data);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={username}
+        placeholder={"아이디"}
+        onChange={userNameHandler}
+      ></input>
+      <input
+        type="password"
+        value={password}
+        placeholder={"비밀번호"}
+        onChange={passwordHandler}
+      ></input>
+      <button onClick={testHandler}>로그인</button>
     </div>
   );
 }
